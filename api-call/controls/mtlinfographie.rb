477e7172be7cdb75b7562c2bc4266ce6e7ae357e
David Guevremont <david.guevremont@videri.com>
2018-03-16 21:12:12 -0400

    InSpec code validation against a JSON file

--- api-call/controls/mtlinfographie.rb
+++ api-call/controls/mtlinfographie.rb
@@ -6,9 +6,10 @@ title 'REST API call learning'
 # Attributes here.
 
 # Plain test here.
-describe http('http://www.mtlinfographie.com') do
-  its('status') { should cmp 200 }
-end
+
+# describe http('http://www.mtlinfographie.com') do
+#   its('status') { should cmp 200 }
+# end
 
 
 # Controls here.
@@ -58,7 +59,40 @@ control 'mtlinfo-04' do
     its('status') { should cmp 200 }
   end
 end
-# read_timeout: 10,
-# open_timeout: 10,
-# ssl_verify: 'fuck',
-# headers: {'Content-Type' => 'application/json'}
+control 'mtlinfo-05' do
+impact 1.0
+title 'JSON configuration validation'
+desc 'Insert some description here'
+ref 'Some reference', url: 'http://link.to.some.documentation.com'
+
+end
+resp = http('https://s3.ca-central-1.amazonaws.com/inspecsyd/test.json', method: 'GET')
+describe 'Country Code' do
+  it 'should eq CA' do
+    expect(json(content: resp.body)['country_code']).to(eq 'CA')
+  end
+ end
+resp = http('https://s3.ca-central-1.amazonaws.com/inspecsyd/test.json', method: 'GET')
+describe 'Encoding' do
+  it 'should eq gzip' do
+    expect(json(content: resp.body)['encoding']).to(eq 'gzip')
+  end
+end
+resp = http('https://s3.ca-central-1.amazonaws.com/inspecsyd/test.json', method: 'GET')
+describe 'IP forwarded' do
+  it 'should eq 127.0.0.1' do
+    expect(json(content: resp.body)['forwarded']).to(eq '127.0.0.1')
+  end
+end
+resp = http('https://s3.ca-central-1.amazonaws.com/inspecsyd/test.json', method: 'GET')
+describe 'Host from the target' do
+  it 'should eq some.random.host.com.' do
+    expect(json(content: resp.body)['host']).to(eq 'some.random.host.com.')
+  end
+end
+resp = http('https://s3.ca-central-1.amazonaws.com/inspecsyd/test.json', method: 'GET')
+describe 'Language returned from the target' do
+  it 'should eq en-US,en;q=0.9' do
+    expect(json(content: resp.body)['lang']).to(eq 'en-US,en;q=0.9')
+  end
+end
